6079ff5bd635f181b645a6bc9f3a41105e2ca0a9
Patrice Douge <patricemorice@yahoo.com>
2019-03-01 13:04:31 -0500

    Imported article text, function for parsing

--- summarizer.py
+++ summarizer.py
@@ -8,7 +8,16 @@ from heapq import nlargest
 from collections import defaultdict
 
 def main():
-    print("Hello world")
+    args = parse_arguments()
+    print(args)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', help='File name of text to summarize')
+    parser.add_argument('-l', '--length', default=4, help='Number of sentences to return')
+    args = parser.parse_args()
+
+    return args
     
 if __name__ == "__main__":
     print(main())
